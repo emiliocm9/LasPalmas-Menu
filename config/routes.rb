9304d1e437f3a9dea37102c6631e9cd976891cd7
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-04 00:25:25 -0500

    CREATE user system

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :sessions
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :users
+  root 'users#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
